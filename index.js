2d3947e47ce9f3efc02e4ea3480b2965b83cfcab
Mark A Guza <guzamaguza@gmail.com>
2019-12-21 02:43:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //collects each pair of numbers with a matching sum
 function bruteForceTwoSum(array, sum){
   let pair = [];
-  let matchingPairs =[][];
-  for(let i=0;i<array.length; i++){
-    for(let j=0;j<array.length;j++){
+  let matchingPairs = [][];
+  for(let i=0;i<array.length-1; i++){
+    for(let j=0;j<array.length-1;j++){
 
       if(sum === array[i] + array[j]){
         pair = [i,j];
