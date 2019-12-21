05dcea75278a0bd47247feb7304c18df7fad1678
Mark A Guza <guzamaguza@gmail.com>
2019-12-21 02:55:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function bruteForceTwoSum(array, sum){
     for(let j=0; j<array.length; j++){
         sum_it = array[i] + array[j]
       if(sum === sum_it){
-        pair = [i,j];
+        pair = [array[i],array[j]];
         matchingPairs.push(pair);
       }
     }
