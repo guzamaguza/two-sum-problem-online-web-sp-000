a8cc8ef878ec8bebb04e04de9c885f3202e3cc9a
Mark A Guza <guzamaguza@gmail.com>
2019-12-21 02:28:07 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+//collects each pair of numbers with a matching sum
+function bruteForceTwoSum(array, sum){
+  for(let i=0;i<array.length; i++){
+    for(let j=0;j<array.length;j++){
+      
+      if(sum == array[i] + array[j]){
+        return 
+      }
+    }
+  }
+}
+
+//collects each pair of numbers with a matching sum
+function binarySearchTwoSum(array, sum){
+
+  
+}
+
+
+//returns true when a match is found
+function binaryMatch(sortedArray, missingNum){
+  
+}
+
+//collects each pair of numbers with a matching sum
+function hashTwoSum(array, sum){
+  
+}
